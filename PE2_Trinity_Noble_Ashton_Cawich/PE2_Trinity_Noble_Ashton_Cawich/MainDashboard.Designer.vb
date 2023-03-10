34da47914e6bb04acdc43bf831dbca1817a2ed6b
Ashtonid <118953491+Ashtonid@users.noreply.github.com>
2023-03-10 13:18:55 -0600

    Add files via upload

---
+++ PE2_Trinity_Noble_Ashton_Cawich/PE2_Trinity_Noble_Ashton_Cawich/MainDashboard.Designer.vb
@@ -0,0 +1,140 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainDashboard
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainDashboard))
+        Me.MenuStrip2 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenForm2ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OptionsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PrintToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearFormToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutSkyWalkerToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutDevelopersToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip2
+        '
+        Me.MenuStrip2.GripMargin = New System.Windows.Forms.Padding(2, 2, 0, 2)
+        Me.MenuStrip2.ImageScalingSize = New System.Drawing.Size(32, 32)
+        Me.MenuStrip2.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.OptionsToolStripMenuItem, Me.AboutToolStripMenuItem})
+        Me.MenuStrip2.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip2.Name = "MenuStrip2"
+        Me.MenuStrip2.Size = New System.Drawing.Size(1608, 42)
+        Me.MenuStrip2.TabIndex = 1
+        Me.MenuStrip2.Text = "MenuStrip2"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OpenForm2ToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Image = CType(resources.GetObject("FileToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(104, 38)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'OpenForm2ToolStripMenuItem
+        '
+        Me.OpenForm2ToolStripMenuItem.Image = CType(resources.GetObject("OpenForm2ToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.OpenForm2ToolStripMenuItem.Name = "OpenForm2ToolStripMenuItem"
+        Me.OpenForm2ToolStripMenuItem.Size = New System.Drawing.Size(290, 44)
+        Me.OpenForm2ToolStripMenuItem.Text = "Open Form 2"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Image = CType(resources.GetObject("ExitToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(290, 44)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'OptionsToolStripMenuItem
+        '
+        Me.OptionsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PrintToolStripMenuItem, Me.ClearFormToolStripMenuItem})
+        Me.OptionsToolStripMenuItem.Image = CType(resources.GetObject("OptionsToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.OptionsToolStripMenuItem.Name = "OptionsToolStripMenuItem"
+        Me.OptionsToolStripMenuItem.Size = New System.Drawing.Size(151, 38)
+        Me.OptionsToolStripMenuItem.Text = "Options"
+        '
+        'PrintToolStripMenuItem
+        '
+        Me.PrintToolStripMenuItem.Image = CType(resources.GetObject("PrintToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.PrintToolStripMenuItem.Name = "PrintToolStripMenuItem"
+        Me.PrintToolStripMenuItem.Size = New System.Drawing.Size(359, 44)
+        Me.PrintToolStripMenuItem.Text = "Print"
+        '
+        'ClearFormToolStripMenuItem
+        '
+        Me.ClearFormToolStripMenuItem.Image = CType(resources.GetObject("ClearFormToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.ClearFormToolStripMenuItem.Name = "ClearFormToolStripMenuItem"
+        Me.ClearFormToolStripMenuItem.Size = New System.Drawing.Size(359, 44)
+        Me.ClearFormToolStripMenuItem.Text = "Clear Form"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutSkyWalkerToolStripMenuItem, Me.AboutDevelopersToolStripMenuItem})
+        Me.AboutToolStripMenuItem.Image = CType(resources.GetObject("AboutToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(132, 38)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'AboutSkyWalkerToolStripMenuItem
+        '
+        Me.AboutSkyWalkerToolStripMenuItem.Name = "AboutSkyWalkerToolStripMenuItem"
+        Me.AboutSkyWalkerToolStripMenuItem.Size = New System.Drawing.Size(359, 44)
+        Me.AboutSkyWalkerToolStripMenuItem.Text = "About SkyWalker"
+        '
+        'AboutDevelopersToolStripMenuItem
+        '
+        Me.AboutDevelopersToolStripMenuItem.Name = "AboutDevelopersToolStripMenuItem"
+        Me.AboutDevelopersToolStripMenuItem.Size = New System.Drawing.Size(359, 44)
+        Me.AboutDevelopersToolStripMenuItem.Text = "About Developers"
+        '
+        'MainDashboard
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1608, 837)
+        Me.Controls.Add(Me.MenuStrip2)
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "MainDashboard"
+        Me.Text = "Main Dashboard"
+        Me.MenuStrip2.ResumeLayout(False)
+        Me.MenuStrip2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip2 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenForm2ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OptionsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PrintToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ClearFormToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutSkyWalkerToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutDevelopersToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ PE2_Trinity_Noble_Ashton_Cawich/PE2_Trinity_Noble_Ashton_Cawich/MainDashboard.vb
@@ -0,0 +1,17 @@
+﻿Public Class MainDashboard
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub AboutSkyWalkerToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutSkyWalkerToolStripMenuItem.Click
+        AboutSkyWalker.Show()
+    End Sub
+
+    Private Sub AboutDevelopersToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutDevelopersToolStripMenuItem.Click
+        AboutDevelopers.Show()
+    End Sub
+End Class
